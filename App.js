9c65c9e7ab698073695f70ed9a80d9656c27305f
wittusthepro <guiditoguida@gmail.com>
2026-04-21 12:31:41 -0500

    Create main React component structure for App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to MED-COIL</h1>
+            {/* Add your components here */}
+        </div>
+    );
+};
+
+export default App;
